1754e2fb2afd8236426d5cdc5441ca3409f39db3
Iosif Turcu <iosif.turcu@emmanuel.ro>
2021-11-12 10:45:07 +0200

    initial setup

---
+++ enums/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
